8f42c0e07b1e08ffe1defc6ce753d95987f996da
dmitrykachala <kosas@mail.ru>
2021-08-13 14:32:51 +0300

    3.3. Разбор первой программы. [#159033]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Dmi3 Kachala");
+        System.out.println("09.05.1983");
+    }
+}
