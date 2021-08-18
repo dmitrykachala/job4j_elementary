0305d5e6b761e4e163cf56eaafc91d12be88efa1
dmitrykachala <kosas@mail.ru>
2021-08-18 12:31:33 +0300

    4. Тесты для стороны прямоугольника. [#159460]

--- test/ru/job4j/conditon/SqAreaTest.java
+++ test/ru/job4j/conditon/SqAreaTest.java
@@ -13,6 +13,7 @@ public class SqAreaTest {
         double out = SqArea.square(p, k);
         Assert.assertEquals(expected, out, 0.01);
     }
+    //dfdfgdfddf
 
     @Test
     public void whenP31K15Square57() {
