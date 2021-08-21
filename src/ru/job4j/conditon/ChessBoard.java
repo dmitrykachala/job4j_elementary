812652a31c72219af3eeb83837bb772817ba62b4
dmitryu.kachala <kosas@mail.ru>
2021-08-22 00:03:10 +0300

    4.3.6. Math.abs [#373616 #192728]

--- src/ru/job4j/conditon/ChessBoard.java
+++ src/ru/job4j/conditon/ChessBoard.java
@@ -5,6 +5,7 @@ package ru.job4j.conditon;
             int rsl = 0;
             if (x1 >= 0 && x1 <= 7 && y1 >= 0 && y1 <= 7
                     && x2 >= 0 && x2 <= 7 && y2 >= 0 && y2 <= 7 ) {
+
                 if (Math.abs(x2 - x1) == Math.abs(y2 - y1)) {
                     rsl = Math.abs(x2 - x1);
                 }
