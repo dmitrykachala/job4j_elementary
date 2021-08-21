59426ef3f2f25689f8bbea2647e4a6f8b8d8f0bb
dmitryu.kachala <kosas@mail.ru>
2021-08-22 00:01:33 +0300

    4.6.2. Switch - matcher [#334071]

--- src/ru/job4j/conditon/ChessBoard.java
+++ src/ru/job4j/conditon/ChessBoard.java
@@ -3,12 +3,13 @@ package ru.job4j.conditon;
     public class ChessBoard {
         public static int way(int x1, int y1, int x2, int y2) {
             int rsl = 0;
-            if ((x1 < 0) || (y1 < 0) || (x2 < 0) || (y2 < 0) || (x1 > 7) || (y1 >7) || (x2 > 7) || (y2 >7) || (x1 != y1) || (x2 !=y2)){
-                return rsl;
+            if (x1 >= 0 && x1 <= 7 && y1 >= 0 && y1 <= 7
+                    && x2 >= 0 && x2 <= 7 && y2 >= 0 && y2 <= 7 ) {
+                if (Math.abs(x2 - x1) == Math.abs(y2 - y1)) {
+                    rsl = Math.abs(x2 - x1);
+                }
             }
-            else {
-                return Math.abs(x2 - x1);
-            }
+            return rsl;
         }
     public static void main(String[] args) {
         System.out.println("Путь, пройденный слоном равен: " + ChessBoard.way(3, 3, 2, 2));
