f8e013829d6b099da15e49cc1fbd4ed1096f6a25
dmitryu.kachala <kosas@mail.ru>
2021-08-21 19:50:45 +0300

    4.3.6. Math.abs [#373616]

---
+++ src/ru/job4j/conditon/ChessBoard.java
@@ -0,0 +1,17 @@
+package ru.job4j.conditon;
+
+    public class ChessBoard {
+        public static int way(int x1, int y1, int x2, int y2) {
+            int rsl = 0;
+            if ((x1 < 0) || (y1 < 0) || (x2 < 0) || (y2 < 0) || (x1 > 7) || (y1 >7) || (x2 > 7) || (y2 >7) || (x1 != y1) || (x2 !=y2)){
+                return rsl;
+            }
+            else {
+                return Math.abs(x2 - x1);
+            }
+        }
+    public static void main(String[] args) {
+        System.out.println("Путь, пройденный слоном равен: " + ChessBoard.way(3, 3, 2, 2));
+        System.out.println("Путь, пройденный слоном равен: " + ChessBoard.way(0, 3, 2, 2));
+    }
+}
