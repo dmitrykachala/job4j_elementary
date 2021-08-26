6a609b0997420d837fac1a1920fc80f3f7e63a9c
dmitrykachala <kosas@mail.ru>
2021-08-26 08:32:19 +0300

    6.4.1.1. Обход массива с последнего элемента [#361619 #194351]

--- src/ru/job4j/array/PrintEvenElements.java
+++ src/ru/job4j/array/PrintEvenElements.java
@@ -9,7 +9,6 @@ public class PrintEvenElements {
                         "Текущий элемент массива, начиная с последнего, с чётным индексом: "
                                 + numbers[numbers.length - 1 - i]
                 );
-                break;
             }
         }
     }
