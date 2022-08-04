334a297e30ea7d0a36e125b858d81ca883023a39
dmitrykachala <kosas@mail.ru>
2022-08-04 17:38:33 +0300

    6.9. String - это массив [#504884]

--- src/ru/job4j/array/JavaNameValidator.java
+++ src/ru/job4j/array/JavaNameValidator.java
@@ -5,25 +5,20 @@ import static java.lang.Character.isDigit;
 public class JavaNameValidator {
 
     public static boolean isNameValid(String name) {
-
         if (name.isEmpty() || !isLowerLatinLetter(name.codePointAt(0))) {
             return false;
         }
-
         for (int i = 1; i < name.length(); i++) {
             int code = name.codePointAt(i);
-            if (isSpecialSymbol(code) || isLowerLatinLetter(code) || isUpperLatinLetter(code)
-                    || isDigit(code)) {
-            } else {
+            if (!(isSpecialSymbol(code) || isLowerLatinLetter(code) || isUpperLatinLetter(code)
+                    || isDigit(code))) {
                 return false;
             }
         }
-
         return true;
     }
 
     public static boolean isSpecialSymbol(int code) {
-
         return code == 36 || code == 95;
     }
 
