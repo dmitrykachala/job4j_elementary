8cb9b3d64304336a7fcb446ef0616ac503e71211
dmitrykachala <kosas@mail.ru>
2022-12-22 11:55:43 +0300

    2. Интерфейс Deque [#504943]

--- src/main/java/ru/job4j/queue/ReconstructPhrase.java
+++ src/main/java/ru/job4j/queue/ReconstructPhrase.java
@@ -21,7 +21,7 @@ public class ReconstructPhrase {
         for (int i = 0; i < count; i++) {
             if (i % 2 == 0) {
                 result.append(evenElements.poll());
-            }else {
+            } else {
                 evenElements.poll();
             }
         }
@@ -31,7 +31,7 @@ public class ReconstructPhrase {
     private String getDescendingElements() {
         StringBuilder result = new StringBuilder();
         while (descendingElements.size() > 0) {
-            result.insert(0,descendingElements.poll());
+            result.insert(0, descendingElements.poll());
         }
         return result.toString();
     }
@@ -40,26 +40,4 @@ public class ReconstructPhrase {
         return  getEvenElements() + getDescendingElements();
     }
 
-    public static void main(String[] args) {
-        Deque<Character> evenElements = new LinkedList<>(
-                Arrays.asList(
-                        'S', '1', 'l', '3', 'o', 'h', 'g', 'h', 'a', 'm', 'n', 't',
-                        ' ', 't', 'o', ' ', 'f', 'q', ' ', '6', 'l', '.', 'a', ',',
-                        'n', 'w', 'g', 'd', 'u', 'a', 'a', 'u', 'g', 'd', 'e', 'q',
-                        ' ', 'y', 'J', 'd', 'a', 'k', 'v', 'p', 'a', 'e', ':', 'b'
-                )
-        );
-        Deque<Character> descendingElements = new LinkedList<>(
-                Arrays.asList(
-                        'e', 'r', 'e', 'h', 'w', 'y', 'r', 'e', 'v', 'e', ' ', 'n', 'u',
-                        'r', ' ', ',', 'e', 'c', 'n', 'o', ' ', 'e', 't', 'i', 'r', 'W', ' '
-                )
-        );
-        String expected = "Slogan of language Java: Write once, run everywhere";
-        ReconstructPhrase rph = new ReconstructPhrase(descendingElements, evenElements);
-        String result = rph.getReconstructPhrase();
-        System.out.println(rph.getEvenElements());
-        System.out.println(rph.getDescendingElements());
-        System.out.println(result);
-    }
 }
