bd68c13e1d75939867f98ab298b278a1b26ec7f4
dmitrykachala <kosas@mail.ru>
2023-01-12 08:43:19 +0300

    9. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,22 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    public static final String NEW_VALUE = "";
+    private final int sizeOfEmpty = 10;
+
+    public String surname;
+    private String name;
+
+    public Broken() { }
+
+    public void echo() { }
+
+    public void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+   public void method(int a, int b, int c, int d, int e, int f, int g) { }
+
+}
