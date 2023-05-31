7e0e134ee4f31583bc1bd8250944df82a73d1f0a
dmitrykachala <kosas@mail.ru>
2023-05-31 11:25:18 +0300

    3. Тестовое задание. Блок "Базовый синтаксис" [#505049]

--- src/test/java/ru/job4j/array/SimpleStringEncoderTest.java
+++ src/test/java/ru/job4j/array/SimpleStringEncoderTest.java
@@ -6,7 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class SimpleStringEncoderTest {
     @Test
-    public void whenStringaaabbcThenResulta3b2c() {
+    void whenStringaaabbcThenResulta3b2c() {
         String input = "aaabbc";
         String expected = "a3b2c";
         String result = SimpleStringEncoder.encode(input);
@@ -14,7 +14,7 @@ public class SimpleStringEncoderTest {
     }
 
     @Test
-    public void whenStringabbcccThenResulta2b3c() {
+    void whenStringabbcccThenResulta2b3c() {
         String input = "abbccc";
         String expected = "ab2c3";
         String result = SimpleStringEncoder.encode(input);
@@ -22,7 +22,7 @@ public class SimpleStringEncoderTest {
     }
 
     @Test
-    public void whenStringaaabccThenResulta3bc2() {
+    void whenStringaaabccThenResulta3bc2() {
         String input = "aaabcc";
         String expected = "a3bc2";
         String result = SimpleStringEncoder.encode(input);
@@ -30,7 +30,7 @@ public class SimpleStringEncoderTest {
     }
 
     @Test
-    public void whenStringabcThenResultabc() {
+    void whenStringabcThenResultabc() {
         String input = "abc";
         String expected = "abc";
         String result = SimpleStringEncoder.encode(input);
@@ -38,7 +38,7 @@ public class SimpleStringEncoderTest {
     }
 
     @Test
-    public void whenStringaaabbbcccThenResulta3b3c3() {
+    void whenStringaaabbbcccThenResulta3b3c3() {
         String input = "aaabbbccc";
         String expected = "a3b3c3";
         String result = SimpleStringEncoder.encode(input);
@@ -46,7 +46,7 @@ public class SimpleStringEncoderTest {
     }
 
     @Test
-    public void whenStringaaaaaaaaaaaabbbcddddThenResulta12b3cd4() {
+    void whenStringaaaaaaaaaaaabbbcddddThenResulta12b3cd4() {
         String input = "aaaaaaaaaaaabbbcdddd";
         String expected = "a12b3cd4";
         String result = SimpleStringEncoder.encode(input);
@@ -54,7 +54,7 @@ public class SimpleStringEncoderTest {
     }
 
     @Test
-    public void whenStringaaabbbbaaabbcccdddddThenResulta3b4a3b2c3d5() {
+    void whenStringaaabbbbaaabbcccdddddThenResulta3b4a3b2c3d5() {
         String input = "aaabbbbaaabbcccddddd";
         String expected = "a3b4a3b2c3d5";
         String result = SimpleStringEncoder.encode(input);
